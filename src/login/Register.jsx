39cf476a81e29c7e96b59475d342df4ab4f74e00
suriyamuthukumarasamy <suriyamuthukumarasamy@gmail.com>
2025-07-22 11:36:22 +0530

    registerfix

--- src/login/Register.jsx
+++ src/login/Register.jsx
@@ -62,9 +62,9 @@ const Register = () => {
         <form onSubmit={handleRegister} style={styles.form}>
           <label htmlFor="fullName" style={styles.label}>Name</label>
           <input
-            id="fullName"
+            id="name"
             type="text"
-            name="fullName"
+            name="name"
             value={formData.name}
             onChange={handleChange}
             placeholder="Enter your name"
